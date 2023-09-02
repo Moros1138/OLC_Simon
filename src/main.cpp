b974b98e9e7cdbb8313dcc094c6cc55ed42f106f
Moros1138 <moros1138@gmail.com>
2023-09-02 16:58:39 -0400

    added SimonButton

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,48 @@
 #include "olcPixelGameEngine.h"
+#include <algorithm>
+#include <random>
+#define BAG_SIZE 100
+
+constexpr int GreenButton  = 0;
+constexpr int RedButton    = 1;
+constexpr int YellowButton = 2;
+constexpr int BlueButton   = 3;
+
+struct SimonButton {
+    
+    SimonButton(olc::vi2d pos, olc::vi2d size, olc::Pixel activeColor, olc::Pixel idleColor, olc::Key key)
+        : pos(pos), size(size), activeColor(activeColor), idleColor(idleColor), key(key)
+    {
+    }
+
+    void Draw(olc::PixelGameEngine* pge)
+    {
+        olc::Pixel color = (active) ? activeColor : idleColor;
+        
+        pge->FillRect(pos, size, color);
+        pge->DrawRect(pos, size, olc::GREY);
+
+        Deactivate();
+    }
+
+    void Activate()
+    {
+        active = true;
+    }
+
+    void Deactivate()
+    {
+        active = false;
+    }
+
+    bool active = false;
+    olc::Pixel activeColor;
+    olc::Pixel idleColor;
+    olc::vi2d pos;
+    olc::vi2d size;
+    olc::Key  key;
+
+};
 
 class OLC_Simon : public olc::PixelGameEngine
 {
