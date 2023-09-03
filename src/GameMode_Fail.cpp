48cdbcf4edb1d74884353eaea31dce8bd426f6ed
Moros1138 <moros1138@gmail.com>
2023-09-03 05:07:24 -0400

    refine the fail screen

--- src/GameMode_Fail.cpp
+++ src/GameMode_Fail.cpp
@@ -13,22 +13,23 @@ void GameMode_Fail::OnCreate(olc::PixelGameEngine* pge)
 
 void GameMode_Fail::OnEnter(olc::PixelGameEngine* pge)
 {
-    state->vecSequence.clear();
-    state->gameIndex = 0;
 }
 
 void GameMode_Fail::OnExit(olc::PixelGameEngine* pge)
 {
+    state->vecSequence.clear();
+    state->gameIndex = 0;
 }
 
 Mode GameMode_Fail::OnUpdate(olc::PixelGameEngine *pge, float fElapsedTime)
 {
     pge->Clear(olc::DARK_BLUE);
 
-    pge->DrawString({ 5, 15 }, std::to_string(state->vecSequence.size()));
-    DrawStringCenter(pge, {pge->ScreenWidth() / 2, pge->ScreenHeight() / 2}, "You have failed!");
-    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 10}, "Press <SPACE> To Try Again");
-    // DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 20}, "Press <C> For Credits");
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) - 32}, "OLC Simon");
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) }, "You remembered " + std::to_string(state->vecSequence.size() - 1) + " this time!");
+    
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 32}, "Press <SPACE> To Try Again");
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 48}, "Press <ESC> to Quit");
 
     if(pge->GetKey(olc::SPACE).bPressed)
         return Mode::ShowSequence;
