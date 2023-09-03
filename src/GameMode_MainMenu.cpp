bc1d81c48cad514b6d3ee121452f476ab8d941f4
Moros1138 <moros1138@gmail.com>
2023-09-03 05:07:32 -0400

    refine the main menu

--- src/GameMode_MainMenu.cpp
+++ src/GameMode_MainMenu.cpp
@@ -23,9 +23,10 @@ Mode GameMode_MainMenu::OnUpdate(olc::PixelGameEngine *pge, float fElapsedTime)
 {
     pge->Clear(olc::DARK_BLUE);
 
-    pge->DrawString({ 5, 15 }, std::to_string(state->vecSequence.size()));
-    DrawStringCenter(pge, {pge->ScreenWidth() / 2, pge->ScreenHeight() / 2}, "OLC Simon");
-    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 10}, "Press <SPACE> To Play");
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) - 32}, "OLC Simon");
+
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 16}, "Press <SPACE> To Play");
+    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 32}, "Press <ESC> to Quit");
 
     if(pge->GetKey(olc::SPACE).bPressed)
         return Mode::ShowSequence;
