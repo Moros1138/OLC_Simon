8654d8317ba02098b5499b1ed4f3a84a8fb84c48
Moros1138 <moros1138@gmail.com>
2023-09-03 01:10:15 -0400

    utilities for drawing strings centered

--- include/Utils.h
+++ include/Utils.h
@@ -2,4 +2,5 @@
 
 #include "olcPixelGameEngine.h"
 
-void DrawStringCenter(olc::PixelGameEngine* pge, olc::vi2d pos, const std::string text, olc::Pixel fgColor = olc::WHITE, olc::Pixel bgColor = olc::BLANK);
+void DrawStringCenter(olc::PixelGameEngine* pge, olc::vi2d pos, std::string text, const olc::Pixel fgColor = olc::WHITE, const olc::Pixel bgColor = olc::BLANK);
+
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,6 +1,6 @@
 #include "Utils.h"
 
-void DrawStringCenter(olc::PixelGameEngine* pge, olc::vi2d pos, const std::string text, olc::Pixel fgColor, olc::Pixel bgColor)
+void DrawStringCenter(olc::PixelGameEngine* pge, olc::vi2d pos, std::string text, const olc::Pixel fgColor, const olc::Pixel bgColor )
 {
     olc::vi2d offset = {
         (text.size() / 2) * 8,
