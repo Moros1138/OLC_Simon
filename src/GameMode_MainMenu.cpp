75846730aa786b20d556b523b3e8d6b63f85c518
Moros1138 <moros1138@gmail.com>
2023-09-03 05:43:48 -0400

    remove escape prompt for quitting

--- src/GameMode_MainMenu.cpp
+++ src/GameMode_MainMenu.cpp
@@ -26,7 +26,7 @@ Mode GameMode_MainMenu::OnUpdate(olc::PixelGameEngine *pge, float fElapsedTime)
     DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) - 32}, "OLC Simon");
 
     DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 16}, "Press <SPACE> To Play");
-    DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 32}, "Press <ESC> to Quit");
+    // DrawStringCenter(pge, {pge->ScreenWidth() / 2, (pge->ScreenHeight() / 2) + 32}, "Press <ESC> to Quit");
 
     if(pge->GetKey(olc::SPACE).bPressed)
         return Mode::ShowSequence;
