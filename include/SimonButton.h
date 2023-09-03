3392247cc84a14e4121977fd468cf636bbe5cf93
Moros1138 <moros1138@gmail.com>
2023-09-02 23:51:31 -0400

    more done with SWE

--- include/SimonButton.h
+++ include/SimonButton.h
@@ -1,20 +1,20 @@
+#pragma once
+
 #include "olcPixelGameEngine.h"
-#include "olcSoundWaveEngine.h"
+
 
 struct SimonButton {
     
-    SimonButton(olc::vi2d pos, olc::vi2d size, olc::Pixel activeColor, olc::Pixel idleColor, olc::Key key, double soundFreq);
+    SimonButton(int id, olc::vi2d pos, olc::vi2d size, olc::Pixel activeColor, olc::Pixel idleColor, olc::Key key);
     void Draw(olc::PixelGameEngine* pge);    
     void Activate();
     void Deactivate();
 
     bool active = false;
+    int id = -1;
     olc::Pixel activeColor;
     olc::Pixel idleColor;
     olc::vi2d pos;
     olc::vi2d size;
     olc::Key  key;
-    
-    double soundFreq;
-    olc::sound::Wave sound;
 };
--- src/SimonButton.cpp
+++ src/SimonButton.cpp
@@ -1,16 +1,8 @@
 #include "SimonButton.h"
 
-SimonButton::SimonButton(olc::vi2d pos, olc::vi2d size, olc::Pixel activeColor, olc::Pixel idleColor, olc::Key key, double soundFreq)
-    : pos(pos), size(size), activeColor(activeColor), idleColor(idleColor), key(key), soundFreq(soundFreq)
+SimonButton::SimonButton(int id, olc::vi2d pos, olc::vi2d size, olc::Pixel activeColor, olc::Pixel idleColor, olc::Key key)
+    : id(id), pos(pos), size(size), activeColor(activeColor), idleColor(idleColor), key(key)
 {
-    sound = olc::sound::Wave(1, sizeof(uint8_t), 44100, 22050);
-    
-    for (size_t i = 0; i < 22050; i++)
-    {
-        double dt = 1 / 44100.0;
-        double t = double(i) * dt;
-        sound.file.data()[i] = float(0.5 * sin(2.0 * soundFreq * 3.14159 * t));
-    }
 }
 
 void SimonButton::Draw(olc::PixelGameEngine* pge)
@@ -18,10 +10,9 @@ void SimonButton::Draw(olc::PixelGameEngine* pge)
     olc::Pixel color = (active) ? activeColor : idleColor;
     
     pge->FillRect(pos, size, color);
-    pge->DrawRect(pos, size, olc::GREY);
-
-    Deactivate();
+    pge->DrawRect(pos, size, (active) ? olc::WHITE : olc::GREY);
 }
+
 
 void SimonButton::Activate()
 {
